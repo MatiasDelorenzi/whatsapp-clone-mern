5bcbdd9238d9f6d33d364e75ac5e07b5c9a5e4d6
unknown <matiasdelorenzi1997@gmail.com>
2022-07-05 00:32:59 -0300

    Setup project

---
+++ frontend/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA6nQnHJ-ap5zpkzLoa12hsqxRXmI8MZcA",
+  authDomain: "whatsapp-mern-cb234.firebaseapp.com",
+  projectId: "whatsapp-mern-cb234",
+  storageBucket: "whatsapp-mern-cb234.appspot.com",
+  messagingSenderId: "775375403817",
+  appId: "1:775375403817:web:599e7d18145ee0854dd538",
+};
